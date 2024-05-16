816017321a25c7c9b2b450a5ae4a489cba7158da
soumyaathanimath <>
2024-05-16 11:21:47 +0530

    using loop patterns

--- src/pattern1/Pattern1.java
+++ src/pattern1/Pattern1.java
@@ -15,14 +15,46 @@ public class Pattern1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         for(int i=1;i<=5;i++)
+         //for(int i=1;i<=5;i++)
+      //{
+        //  for(int j=1;j<=5;j++)
+          //{
+            //System.out.println(j+" ");
+          //}
+          //System.out.println("");
+      //}
+    //}
+    //}
+     //for(int i=1;i<=5;i++)
+      //{
+        //  for(int j=1;j<=5;j++)
+          //{
+            //System.out.println(i+" ");
+          //}
+          //System.out.println("");
+      //}
+    //}
+    
+ //for(int i=1;i<=5;i++)
+   //   {
+     //     for(int j=1;j<=5;j++)
+       //   {
+         //   System.out.println(i+j+" ");
+          //}
+          //System.out.println("");
+      //}
+    //}
+//}
+ int count=0;
+        
+      for(int i=1;i<=5;i++)
       {
           for(int j=1;j<=5;j++)
           {
-            System.out.println(j+" ");
+              count++;
+            System.out.format("%02d ",count);
           }
           System.out.println("");
       }
     }
-    }
-    
+}
