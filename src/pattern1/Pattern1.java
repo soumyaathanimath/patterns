91673ba4a4c51ae0bf2ae30127f3e6e1e3f8a9ac
soumyaathanimath <>
2024-05-16 10:56:31 +0530

    number pattern

---
+++ src/pattern1/Pattern1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pattern1;
+
+/**
+ *
+ * @author dell1
+ */
+public class Pattern1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         for(int i=1;i<=5;i++)
+      {
+          for(int j=1;j<=5;j++)
+          {
+            System.out.println(j+" ");
+          }
+          System.out.println("");
+      }
+    }
+    }
+    
